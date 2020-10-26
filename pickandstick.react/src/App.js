434f22a5ac690fd4f7ddde8e60432dca76eb7159
Grant <grantmtabor@gmail.com>
2020-10-26 11:21:12 -0500

    connecting DB to server

--- pickandstick.react/src/App.js
+++ pickandstick.react/src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const register = (username, password) =>{
+    console.log("registered");
+  }
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      hello world
+      testing
+      <input placeholder="Enter username" onChange={e => setUsername(e.target.value)} />
+      <input placeholder="Enter password" onChange={e => setPassword(e.target.value)} />
+
+
+
+      {username}
+      <button onClick={() => register(username, password)} >Submit</button>
     </div>
   );
 }
